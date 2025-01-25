cb966c61df480361a745a167f07efed618eb6d8a
Robin Watson <robin@robin-watson.com>
2025-01-25 16:53:00 +0000

    Actually Day 6 Other day 6 was a typo and meant to be day 5 Mostly took place on "reeborg's world"

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,12 @@
+# print("Hello")
+# print(len("Hello"))
+
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+#
+# my_function()
+
+# This next bit of the course is done on another website
+# Learnt how to use while loops on the website
+
