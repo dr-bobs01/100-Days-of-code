dd22a14bcb2e8a5810ae942dbdd05253936b5b4d
Robin Watson <robin@robin-watson.com>
2025-03-22 16:58:38 +0000

    Day 13 Learnt debugging

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,7 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("Please only numbers e.g. 7")
+    age = int(input("How old are you?"))
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
