2824833ffcd2c5c07206e9108e1b4ca07c7e0b78
Robin Watson <robin@robin-watson.com>
2025-04-05 17:38:11 +0100

    Day 15 Made a coffee machine

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -28,5 +28,63 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "money": 0,
 }
 
+def report():
+    print(f"""Water: {resources["water"]}ml
+Milk: {resources["milk"]}ml
+Coffee: {resources["coffee"]}g
+Money: ${resources["money"]}""")
+
+def res_check():
+    if MENU[request]["ingredients"]["water"] < resources["water"]:
+        try:
+            if MENU[request]["ingredients"]["milk"] < resources["milk"]:
+                if MENU[request]["ingredients"]["coffee"] < resources["coffee"]:
+                    return True
+            else:
+                return "milk"
+        except KeyError:
+            if MENU[request]["ingredients"]["coffee"] < resources["coffee"]:
+                return True
+        else:
+            return "coffee"
+    else:
+        return "water"
+
+def money():
+    print("Please insert coins.")
+    q = int(input("How many quarters?: "))
+    d = int(input("How many dimes?: "))
+    n = int(input("How many nickles?: "))
+    p = int(input("How many pennies?: "))
+    change = (MENU[request]["cost"]-(q * 0.25)-(d * 0.10)-(n*0.05)-(p*0.01))
+    if change <= 0:
+        print(f"Here is ${round(change * -1,2)} in change.")
+        resources["water"] -= MENU[request]["ingredients"]["water"]
+        try:
+            resources["milk"] -= MENU[request]["ingredients"]["milk"]
+        except KeyError:
+            5 + 5 # i needed a line of code that did nothing for the code to work
+        resources["coffee"] -= MENU[request]["ingredients"]["coffee"]
+        resources["money"] += MENU[request]["cost"]
+        print(f"Here is your {request} ☕️. Enjoy!")
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+
+on = True
+while on:
+    request = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if request == "report":
+        report()
+    elif request == "off":
+        exit()
+    ## do a bunch of checks if you have enough resources
+    if request in MENU:
+        temp = res_check()
+        if temp == True:
+            ## this is where money check goes
+            money()
+        else:
+            print(f"Sorry there is not enough {temp}.")
