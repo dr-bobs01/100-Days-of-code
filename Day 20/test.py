5ed1e8853e88da55257b1b2f8c5f06e8d35a61a1
Robin Watson <robin@robin-watson.com>
2025-05-10 18:54:24 +0100

    Day 20 Did Part 1 of snake Basic movement of snake

---
+++ Day 20/test.py
@@ -0,0 +1,10 @@
+# class Cheese:
+#     def mcguffin(self,cheese):
+#         print(f"test {cheese}")
+#
+# mozzerella = Cheese()
+#
+# mozzerella.mcguffin("potato")
+
+from snake_game_ import Snake
+
