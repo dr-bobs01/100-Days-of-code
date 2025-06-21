2ca9ccbcb25095b436bc283089e372e9175cde57
Robin Watson <robin@robin-watson.com>
2025-06-21 18:42:19 +0100

    Day 26 Learnt list and dictionary comprehension made a nato phonetic speller

---
+++ Day 26/main.py
@@ -0,0 +1,13 @@
+## list comprehension
+## make new list from old list
+## new_list = [new_item for item in list]
+## make new list from old list when condition
+## new_list = [new_item for item in list if test]
+
+## Dictionary Comprehension
+## simplest
+## new_dict = {new_key:new_value for item in list}
+## new dictionary from old one
+## new_dict = {new_key:new_value for (key,value) in dict.items()}
+## conditional, replace "dict" not "items" for "in ...."
+## new_dict = {new_key:new_value for (key,value) in dict.items() if test}
